48bc1f1f18ce2c08a245ccc362541ae65378f4db
Michael Tranquillo <michael.r.tranquillo@gmail.com>
2023-04-14 12:34:12 -0400

    created empty strings

--- Develop/script.js
+++ Develop/script.js
@@ -1,8 +1,10 @@
 // Assignment code here
 let lowerCase = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
 let upperCase = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
-let specialCase = [' ', '!', `"`, '#', '$', '%', '&', `'`, '(', ')', '*', '+', ',', '-', '.', '/', ':', ';', '<', '=', '>', '?', '@', '[', `\\`, ']', '^', '_', '`', '{', '|', '}', '~', ];
-let numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', ];
+let specialCase = [' ', '!', `"`, '#', '$', '%', '&', `'`, '(', ')', '*', '+', ',', '-', '.', '/', ':', ';', '<', '=', '>', '?', '@', '[', `\\\\`, ']', '^', '_', '`', '{', '|', '}', '~'];
+let numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+let finalPassword = str();
+let selectionStr = str();
 
 function generatePassword() {
 
@@ -11,7 +13,6 @@ function generatePassword() {
   return password
 }
 
-console.log(specialCase);
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
