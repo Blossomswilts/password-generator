cd0c969c391ac486d8f71d7d32bff8fe5fc98127
Michael Tranquillo <michael.r.tranquillo@gmail.com>
2023-04-14 13:55:56 -0400

    created pseudo code

--- Develop/script.js
+++ Develop/script.js
@@ -1,16 +1,32 @@
 // Assignment code here
-let lowerCase = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
-let upperCase = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
-let specialCase = [' ', '!', `"`, '#', '$', '%', '&', `'`, '(', ')', '*', '+', ',', '-', '.', '/', ':', ';', '<', '=', '>', '?', '@', '[', `\\\\`, ']', '^', '_', '`', '{', '|', '}', '~'];
-let numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
-let finalPassword = str();
-let selectionStr = str();
 
-function generatePassword() {
+// let finalPassword = str();
+// let selectionStr = str();
+
 
 
 
-  return password
+function generatePassword() {
+  // Create used variables
+  const lowerCase =   ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+  const upperCase =   ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
+  const specialCase = [' ', '!', `"`, '#', '$', '%', '&', `'`, '(', ')', '*', '+', ',', '-', '.', '/', ':', ';', '<', '=', '>', '?', '@', '[', `\\\\`, ']', '^', '_', '`', '{', '|', '}', '~'];
+  const numbers =     ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+
+//      Create prompts for password criteria
+// Create password input between or equal to 8 & 128 characters.
+// Do you want uppercase characters?
+// Do you want lowercase Characters?
+// Do you want numbers?
+// Do you want spcial characters?
+
+// Validate that at least one of the selections is true and that the characters are between set numbers. 
+// Password would be made when validated. 
+
+
+
+//Display password in an alert or written to the page. 
+  return "password"
 }
 
 
