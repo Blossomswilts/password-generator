6cfb5bc4a721d404db97d3843ef7459f69e305a6
Michael Tranquillo <michael.r.tranquillo@gmail.com>
2023-04-15 17:19:05 -0400

    reformatting nests and retrying systems

--- Develop/script.js
+++ Develop/script.js
@@ -13,44 +13,52 @@ function generatePassword() {
   let selectionStr = "";
 
 //      Create prompts for password criteria
+//------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 // Create password input between or equal to 8 & 128 characters.
-let passwordLength = prompt("Chose a number between 8 and 128.");
-if (passwordLength >= 8 && passwordLength <= 128) {
-  // Do you want uppercase characters?
-    let caseUpper = confirm("Do you want any upper case characters?"); 
-  // Do you want lowercase Characters?
-    let caseLower = confirm("Do you want any lower case characters?");
-  // Do you want numbers?
-    let caseNumber = confirm("Do you want any numbers?");
-  // Do you want spcial characters?
-    let caseSpecial = confirm("Do you want any special case characters?");
+let passwordLength = prompt("Choose a number between 8 and 128.");
+passwordLength = parseInt(passwordLength);
 
-} else {
-  alert("Please chose a number between 8 and 128.")
+//Alerts user if password isn't within accepted range.
+  if (passwordLength < 8 || passwordLength > 128) {
+    alert("Please choose a number between 8 and 128.");
+    return "";
+  }
+  
+//------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+  // Do you want uppercase characters?
+let caseUpper = confirm("Do you want any upper case characters?"); 
+  if (caseUpper) {
+    selectionStr =+ lowerCase;
+  }
+
+  // Do you want lowercase Characters?
+let caseLower = confirm("Do you want any lower case characters?");
+  if (caseLower) {
+    selectionStr =+ lowerCase;
+  }
+  
+  // Do you want numbers?
+let caseNumber = confirm("Do you want any numbers?");
+if (caseNumber) {
+  selectionStr =+ numbers;
 }
 
+  // Do you want spcial characters?
+let caseSpecial = confirm("Do you want any special case characters?");
+if (caseSpecial) {
+  selectionStr =+ specialCase;
+}
 
+console.log(selectionStr);
+
+  
+
+
+//------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 // Validate that at least one of the selections is true and that the characters are between set numbers. 
-if (caseUpper === true) {
-  for (var i = 0; i <= passwordLength.length; i++) {
-    selectionStr += i(Math.floor(Math.random() * caseUpper.length));
-  }
-}
-if (caseLower === true) {
-  for (var i = 0; i <= passwordLength.length; i++);
-}
-if (caseNumber === true) {
-  for (var i = 0; i <= passwordLength.length; i++);
-}
-if (caseSpecial === true) {
-  for (var i = 0; i <= passwordLength.length; i++);
-}
-// Password would be made when validated. 
 
-
-//Display password in an alert or written to the page. 
   return finalPassword
 }
 
